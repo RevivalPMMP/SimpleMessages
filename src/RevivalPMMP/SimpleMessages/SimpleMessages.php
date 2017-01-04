e6cc1de0f8955c6a8db8eb6fec38bdb1aef0cfbc
Magicode <herbmcsherb@gmail.com>
2017-01-04 13:47:41 -0500

    Get the Plugin Up
    Haven't tested it yet - I will tonight.

---
+++ src/RevivalPMMP/SimpleMessages/SimpleMessages.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace RevivalPMMP\SimpleMessages;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+
+
+class SimpleMessages extends PluginBase{
+    public function onEnable(){
+        $this->getLogger()->info("Loading SimpleMessages!");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+	$this->reloadConfig();
+    }
+
+    public function onDisable(){
+        $this->getLogger()->info("Disabling SimpleMessages!");
+    }
+    
+    function onJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $name = $player->getDisplayName();
+        Server::getInstance()->broadcastMessage("".$name. $this->getConfig()->get("Join-Message"));
+
+}
+    function onQuit(PlayerQuitEvent $event) {
+        $player = $event->getPlayer();
+        $name = $player->getDisplayName();
+        Server::getInstance()->broadcastMessage("".$name. $this->getConfig()->get("Leave-Message"));
+        }
+}
+
